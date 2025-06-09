ebeee9eb03ef9835f1b7f77933c6e13086267bb3
Alex Lanção <lancao.af@gmail.com>
2025-06-09 02:37:36 -0300

    refactor: update database settings to use environment variables and clean up requirements files

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 def main():
     
     BASE_DIR = Path(__file__).resolve().parent.parent
-    dotenv_path = BASE_DIR / 'web3integration' / '.env'
+    dotenv_path = BASE_DIR / '.env'
     load_dotenv(dotenv_path)
 
     """Run administrative tasks."""
